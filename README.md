60ba6d4f0ef514ba67b69c0d62b0edeb2cb1faa2
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2024-08-18 06:47:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+LibraryHub - Online eBook/Notes Platform: (MERN Stack )
+
+Link: https://libraryhub16.netlify.app/
+
+![image](https://github.com/bellacaio16/LibraryHub/assets/147348222/5494ce2d-b101-4403-979a-9ae7be54eeab)
+
---
+++ server/constants/products-data.js
@@ -0,0 +1,144 @@
+export const productsData = [
+  {
+    id: "product1",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:288:400:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9789395483247.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:288:400:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9781642021677.jpg",
+    title: {
+      shortTitle: "Modi@20",
+      longTitle: "Modi@20",
+    },
+    price: {
+      mrp: 625,
+      cost: 562,
+      discount: "47%",
+    },
+    quantity: 1,
+    description:
+      "Modi@20 Book.",
+    discount: "Extra 10% Off",
+    tagline: "Deal of the day",
+  },
+  {
+    id: "product2",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:288:400:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9781642021677.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:288:400:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9781642021677.jpg",
+    title: {
+      shortTitle: "My Name Is Alison",
+      longTitle: "My Name Is Alison: An Urban Fantasy Action Adventure: 3 (Alison Brownstone)",
+    },
+    price: {
+      mrp: 899,
+      cost: 599,
+      discount: "40%",
+    },
+    quantity: 1,
+    description:
+      "My Name Is Alison: An Urban Fantasy Action Adventure: 3 (Alison Brownstone). ",
+    discount: "From 99+5% Off",
+    tagline: "Pestige, Nova & more",
+  },
+  {
+    id: "product3",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9780692162682.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9780692162682.jpg",
+    title: {
+      shortTitle: "Out of Darkness",
+      longTitle:
+        "Out of Darkness shared By M A Richards",
+    },
+    price: {
+      mrp: 499,
+      cost: 366,
+      discount: "66%",
+    },
+    quantity: 1,
+    description:
+      "Out of Darkness shared By M A Richards.",
+    discount: "Upto 70% Off",
+    tagline: "Deal of the Day",
+  },
+  {
+    id: "product4",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9788183227001.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9788183227001.jpg",
+    title: {
+      shortTitle: "The 1% Solution- for Work and Life",
+      longTitle: "The 1% Solution- for Work and Life",
+    },
+    price: {
+      mrp: 999,
+      cost: 889,
+      discount: "42%",
+    },
+    quantity: 1,
+    description:
+      "The 1% Solution- for Work and Life.",
+    discount: "Grab Now",
+    tagline: "Best Seller",
+  },
+  {
+    id: "product5",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9789355995001.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9789355995001.jpg",
+    title: {
+      shortTitle: "Ikigai ",
+      longTitle: "Ikigai : Japanese Art of staying Young.. While growing Old",
+    },
+    price: {
+      mrp: 799,
+      cost: 624,
+      discount: "40%",
+    },
+    quantity: 1,
+    description: "Ikigai : Japanese Art of staying Young.. While growing Old",
+    discount: "From ₹499",
+    tagline: "Kubra, Nova & more",
+  },
+  {
+    id: "product6",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9781612681139.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fbookscape-s3-bucket.s3.amazonaws.com%2Fproduct_images%2F9781612681139.jpg",
+    title: {
+      shortTitle: "Rich Dad Poor Dad",
+      longTitle:
+        "Rich Dad Poor Dad",
+    },
+    price: {
+      mrp: 890,
+      cost: 720,
+      discount: "46%",
+    },
+    quantity: 1,
+    description:
+      'Rich Dad Poor Dad , shared What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not! By Robert T. Kiyosaki ',
+    discount: "Minimum 40% Off",
+    tagline: "Top Selling",
+  },
+  {
+    id: "product7",
+    url: "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fimage-hub.reproindialtd.com%2F9798890665867.jpg",
+    detailUrl:
+      "https://d34a0mln2492j4.cloudfront.net/unsigned/resize:fit:760:1000:0/gravity:sm/plain/https%3A%2F%2Fimage-hub.reproindialtd.com%2F9798890665867.jpg",
+    title: {
+      shortTitle: "Money Mindsets",
+      longTitle:
+        "Money Mindset: Science-Based Stories to Rewire your Money Beliefs Goals & Habits ",
+    },
+    price: {
+      mrp: 690,
+      cost: 599,
+      discount: "59%",
+    },
+    quantity: 1,
+    description:
+      "Money Mindset: Science-Based Stories to Rewire your Money Beliefs Goals & Habits.",
+    discount: "Minimum 50% Off",
+    tagline: "Grab Now!",
+  },
+];
